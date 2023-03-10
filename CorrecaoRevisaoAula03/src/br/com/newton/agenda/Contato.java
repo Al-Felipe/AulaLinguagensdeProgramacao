2ba0879d6e4e0fac482381c9f102d5d5517c8448
Al-Felipe <105646899+Al-Felipe@users.noreply.github.com>
2023-03-09 21:02:34 -0300

    Adiciona atributos a classe contato

--- CorrecaoRevisaoAula03/src/br/com/newton/agenda/Contato.java
+++ CorrecaoRevisaoAula03/src/br/com/newton/agenda/Contato.java
@@ -1,4 +1,32 @@
 package br.com.newton.agenda;
 
 public class Contato {
+    private String nome;
+    private String numero;
+
+//    Metodo para excluir contato que recebe como paramentro o contato a ser excluido
+    public void Excluir(Contato contato) {
+        System.out.println("Excluindo o contato " + contato.nome);
+    }
+
+    public void incluirContato(Contato contato){
+        System.out.println("Incluindo o contato: " + contato.nome);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
 }
--- CorrecaoRevisaoAula03/src/br/com/newton/main/Telefone.java
+++ CorrecaoRevisaoAula03/src/br/com/newton/main/Telefone.java
@@ -4,16 +4,22 @@ import br.com.newton.agenda.Contato;
 
 public class Telefone {
     public static void main(String[] args) {
-        Contato infoContato = new Contato();
+        Contato contato = new Contato();
 
         Scanner input = new Scanner(System.in);
         System.out.println("Digite o nome de contato:");
         String nome = input.nextLine();
+        contato.setNome(nome);
 
         System.out.println("Digite o numero do seu telefone: ");
         String numTelefone = input.nextLine();
+        contato.setNumero(numTelefone);
+
+        contato.Excluir(contato);
+        contato.incluirContato(contato);
 
         //verifica se o numero do telefone é valido
-        if(numTelefone.length() != 9 ) System.out.println("Telefone invalido");
+        if(contato.getNumero().length() != 9 ) System.out.println("Telefone invalido");
+
     }
 }
